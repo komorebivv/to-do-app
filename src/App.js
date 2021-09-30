bbe17c44103456d63ae67fe010c6258d964215ad
komorebivv <patrycja.stanislawczyk@gmail.com>
2021-09-30 12:26:16 +0200

    change title

--- src/App.js
+++ src/App.js
@@ -26,34 +26,3 @@ function App() {
 }
 
 export default App;
-
-
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Countries from './components/Countries';
-
-// function App() {
-//   return (
-//     // <div className="App">
-//     // <Carousel />
-
-//     // </div>
-//     <Router>
-//       <Navigation />
-//       <Switch>
-//     <Route exact path="/">
-//       <Carousel />
-//     </Route>
-//     <Route exact path="/about">
-//       <About />
-//     </Route>
-//     <Route exact path="/trivias">
-//       <Trivias />
-//     </Route>
-//     <Route exact path="/trivia/:id">
-//       <Trivia />
-//       </Route >
-//     <Route exact path="/countries">
-//     <Countries />
-//     </Route>
-//       </Switch>
-//     </Router>
